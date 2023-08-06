ab7de2a96df51a63906f059998401a516d5e5692
BrailyRS <brailyrs03@gmail.com>
2023-08-06 14:19:42 -0400

    Generador de contraseñas 'completo'´.

---
+++ Readme.md
@@ -0,0 +1,57 @@
+# Generador de Contraseñas Aleatorias
+
+Este es un programa de Python que te permite generar contraseñas aleatorias con opciones de configuración. Puedes determinar la longitud de la contraseña, incluir o excluir letras mayúsculas, números y símbolos.
+
+------------
+
+## Instrucciones
+
+1. **Ejecución:** Simplemente ejecuta el script en Python para generar una nueva contraseña aleatoria.
+
+2. **Configuración:** El programa te guiará para configurar los parámetros de generación de la contraseña:
+
+   - **Longitud:** Ingresa un número entero entre 8 y 16 para determinar la longitud de la contraseña.
+   - **Letras mayúsculas:** Responde 'y' para incluir letras mayúsculas, o 'n' para excluirlas.
+   - **Números:** Responde 'y' para incluir números, o 'n' para excluirlos.
+   - **Símbolos:** Responde 'y' para incluir símbolos, o 'n' para excluirlos.
+
+3. **Contraseña Generada:** El programa mostrará la nueva contraseña generada según las configuraciones seleccionadas.
+
+## Ejemplo de Uso
+
+
+>Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+> Podrás configurar generar contraseñas con los siguientes parámetros:
+> - Longitud: Entre 8 y 16.
+> - Con o sin letras mayúsculas. 
+> - Con o sin números. 
+> - Con o sin símbolos. 
+> (Pudiendo combinar todos estos parámetros entre ellos) 
+>
+>Ingrese la longitud de su contraseña (entre 8 y 16): 12
+>
+> Para continuar ingrese [y/n] 
+>
+>
+>Desea que su contraseña contenga letras mayúsculas? [y/n]: y
+>
+>✅ Siga con el proceso ✅
+>
+>Que sea que su contraseña contenga números? [y/n]: y
+>
+>✅ Siga con el proceso ✅
+>
+>Desea que su contraseña contenga símbolos? [y/n]: y
+>
+>✅ Siga con el proceso ✅
+>
+>Tu nueva contraseña contiene 12 caracteres.
+Esta es tu nueva contraseña: 🔐 F8$zY3&rL9@A 🔐
+
+
+------------
+
+## Notas
+
+- Asegúrate de tener Python instalado en tu sistema para ejecutar el programa.
+- La contraseña generada es completamente aleatoria y segura, asegúrate de guardarla en un lugar seguro.
--- generador_pass.py
+++ generador_pass.py
@@ -1,9 +1,10 @@
 import random
 import sys
 
+# Mensaje de presentación y descripción del programa.
 print("""
   +=============================================================================+
-  |Escribe un programa que sea capaz de generar contraseñas de forma aleatoria. |
+  | Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.|
   | Podrás configurar generar contraseñas con los siguientes parámetros:        |
   | - Longitud: Entre 8 y 16.                                                   |
   | - Con o sin letras mayúsculas.                                              |
@@ -11,90 +12,122 @@ print("""
   | - Con o sin símbolos.                                                       |
   | (Pudiendo combinar todos estos parámetros entre ellos)                      |
   +=============================================================================+
- """)
-#=====================================Listas==========================================================================
-lista_letras_minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+""")
 
-lista_letras_mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-lista_simbolos = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '/', '?']
-
-lista_numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-#=====================================================================================================================
-condicion = 0
-
-password : list = [] 
-
-proceso = "✅Siga con el proceso✅\n"
-
-
-def validation_long_pass():
-  """Esta funcion como su nombre indica valida la longitud de la contraseña que usuario queiere generar."""
-  while condicion < 1:
-    valor_verficacion_log = long_pass()
-    cont_log = valor_verficacion_log
-    if cont_log >= 8 and cont_log <= 16:
-      print(proceso)
-      break
-    elif cont_log < 8 or cont_log > 16:
-      print(f"Porfavor ingrese un valor mayor a 8 y menor que 16")
-  return cont_log
-
+def main():
+    print(f"Tu nueva contraseña contiene {len(password)} caracteres.")
+    print(f"Esta es tu nueva contraseña: 🔐 {password} 🔐")
 
 def long_pass() -> int:
-  """Esta funcion hace que el usuario determine la longitud de la contraseña que quiere generar."""
-  usuario_longitud : int = int(input("Ingresa la longitud de tu contraseña (no sea mayor a 16 pero que sea igual o mayor a 8) -->"))
-  return usuario_longitud
+    """Permite al usuario determinar la longitud de la contraseña que quiere generar (entero)."""
+    usuario_longitud = int(input("Ingrese la longitud de su contraseña (entre 8 y 16): "))
+    return usuario_longitud
+
+def validation_long_pass():
+    """Valida la longitud de la contraseña que el usuario quiere generar."""
+    while condicion < 1:
+        valor_verificacion_long = long_pass()
+        cont_log = valor_verificacion_long
+        if cont_log >= 8 and cont_log <= 16:
+            print(proceso)
+            break
+        elif cont_log < 8 or cont_log > 16:
+            print(f"Por favor, ingrese un número entero mayor a 8 y menor que 16.")
+    return cont_log
+
+def enter_mayus(lista_letras):
+    """Agrega letras mayúsculas o minúsculas a la contraseña, según la elección del usuario."""
+    if usuario_mayus is True:
+        password.append(random.choice(lista_letras))
+    else:
+        password.append(random.choice(lista_letras_minusculas))
+    return password
+
+def enter_number(lista_numeros):
+    """Agrega números a la contraseña si el usuario lo desea."""
+    if usuario_numeros is True:
+        password.append(random.choice(lista_numeros))
+    return password
+
+def enter_simbolos(lista_simbolos):
+    """Agrega símbolos a la contraseña si el usuario lo desea."""
+    if usuario_simbolos is True:
+        password.append(random.choice(lista_simbolos))
+    return password
+
+# Listas con posibles opciones para la generación de contraseñas.
+lista_letras_minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+lista_letras_mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+lista_simbolos = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '/', '?']
+lista_numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+condicion = 0
+password: list = []
+proceso = "✅ Siga con el proceso ✅\n"
+error = "⚠️ Error: Ingrese valores válidos. ⚠️"
 
 try:
-  validacion_cofirmada = validation_long_pass()
+    # Validación de que el usuario ingrese correctamente la longitud deseada.
+    validacion_confirmada = validation_long_pass()
 except ValueError:
-  print("⚠️ Este programa no acepta letras⚠️, por ahora el programa no podra seguir.")
-  sys.exit()
+    print("⚠️ Error al ejecutar el programa. ⚠️\nEl programa no podrá continuar.")
+    sys.exit()
 
-usuario_mayus : str = input(f"Desea que su contraseña contenga letras mayusculas? ").lower()
+print("""
+      +==============================+
+      | Para continuar ingrese [y/n] |
+      +==============================+
+      """)
 
-if usuario_mayus == "si" or usuario_mayus == "yes":
-  usuario_mayus = True
-else:
-  usuario_mayus = False
-print(proceso)
-
-usuario_numeros : str = input(f"Desea que su contraseña contenga numeros? ").lower
-if usuario_numeros == "si" or usuario_numeros == "yes":
-  usuario_numeros = True
-else:
-  usuario_numeros = False
-print(proceso)
-
-usuario_simbolos : str = input(f"Desaaea que su contraseña contenga simbolos? ").lower()
-if usuario_simbolos == "si" or usuario_simbolos == "yes":
-  usuario_simbolos = True
-else:
-  usuario_simbolos = False
-print(proceso)
-
-def enter_mayus():
-    if usuario_mayus is True:
-      password.append(random.choice(lista_letras_mayusculas))
+while condicion == 0:
+    usuario_mayus = input(f"Desea que su contraseña contenga letras mayúsculas? [y/n]: ").lower()
+    if usuario_mayus == "y":
+        usuario_mayus = True
+        print(proceso)
+        break
+    elif usuario_mayus == "n":
+        usuario_mayus = False
+        print(proceso)
+        break
     else:
-      password.append(random.choice(lista_letras_minusculas))
-    return password
+        print(error)
 
-def enter_number():
-    if usuario_numeros is True:
-      password.append(random.choice(lista_numeros))
-      return password
+while condicion == 0:
+    usuario_numeros = input(f"Desea que su contraseña contenga números? [y/n]: ").lower()
+    if usuario_numeros == "y":
+        usuario_numeros = True
+        print(proceso)
+        break
+    elif usuario_numeros == "n":
+        usuario_numeros = False
+        print(proceso)
+        break
+    else:
+        print(error)
 
-def enter_simbolos():
-    if usuario_simbolos is True:
-     password.append(random.choice(lista_simbolos))
-    return password
+while condicion == 0:
+    usuario_simbolos = input(f"Desea que su contraseña contenga símbolos? [y/n]: ").lower()
+    if usuario_simbolos == "y":
+        usuario_simbolos = True
+        print(proceso)
+        break
+    elif usuario_simbolos == "n":
+        usuario_simbolos = False
+        print(proceso)
+        break
+    else:
+        print(error)
 
-for x in range(validacion_cofirmada):
-  enter_mayus()
-  enter_number()
-  enter_simbolos()
-  
+# Generar la contraseña aleatoria con los parámetros configurados por el usuario.
+for _ in range(validacion_confirmada):
+    if len(password) < validacion_confirmada:
+        enter_mayus(lista_letras_mayusculas)
+    if len(password) < validacion_confirmada:
+        enter_number(lista_numeros)
+    if len(password) < validacion_confirmada:
+        enter_simbolos(lista_simbolos)
+
+# Convertir la lista de contraseñas en una sola cadena y mostrar la contraseña generada.
 password = "".join(password)
-print(f"Tu nueva contraseña contiene {len(password)} caracteres.\nEsta es tu nueva contraseña 🔐--> {password} <--🔐")
+
+main()
